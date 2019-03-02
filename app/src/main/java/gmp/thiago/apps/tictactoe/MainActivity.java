b4ca2c92cffbdb0c5c052001be13f9b7ada07a83
thimarcal <thiago.gm.pereira@gmail.com>
2019-03-02 19:12:47 -0300

    Fixed problem with Keyboard focus

--- app/src/main/java/gmp/thiago/apps/tictactoe/MainActivity.java
+++ app/src/main/java/gmp/thiago/apps/tictactoe/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -58,12 +59,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             greetingsTv.setVisibility(View.INVISIBLE);
             playerNameEt.setVisibility(View.VISIBLE);
             changeNameBtn.setVisibility(View.INVISIBLE);
-            InputMethodManager imm = (InputMethodManager)   getSystemService(INPUT_METHOD_SERVICE);
-            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
         }
 
         startGameBtn.setOnClickListener(this);
         changeNameBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
     }
 
     @Override
